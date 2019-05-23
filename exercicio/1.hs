b5166a215cc911574718a49be93cad3be0a34577
renanbaqui <46902597+renanbaqui@users.noreply.github.com>
2019-05-23 00:56:42 -0300

    Update and rename trees.hs to 1.hs

--- exercicio/1.hs
+++ exercicio/1.hs
@@ -19,7 +19,9 @@ altura (Galho e d) = if (altura e) > (altura d)
 
 espelho :: Arvore -> Arvore
 espelho Folha       = Folha
-espelho (Galho e d) = (Galho d e)
+espelho (Galho e d) = Galho (espelho d) (espelho e)
 
 
-main = print (espelho a3)
+main = do print (folhas a3)
+          print (altura a3)
+          print (espelho a3)
